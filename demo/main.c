094f7d5b32f976c5bd4a5eb3b9b4baf589a7d665
marciovmf <marciovmf@gmail.com>
2024-02-12 19:00:32 +0100

    Improved the engine demo free camera

--- demo/main.c
+++ demo/main.c
@@ -3,8 +3,8 @@
 #include "ldk/asset/mesh.h"
 #include "ldk/maths.h"
 #include "ldk/common.h"
-
-//TODO: Create Events for Joystick button and axis input
+#include "ldk/maths.h"
+#include <math.h>
 
 typedef struct
 {
@@ -15,6 +15,7 @@ typedef struct
   uint64 ticksEnd;
   float deltaTime;
   bool looking;
+  bool strafing;
 } GameState;
 
 // Pure engine approach
@@ -57,14 +58,13 @@ bool onKeyboardEvent(const LDKEvent* event, void* data)
       state->camera->position = vec3Sub(state->camera->position, vec3Mul(side_dir, speed));
       state->camera->target = vec3Sub(state->camera->target, vec3Mul(side_dir, speed));
     }
-
   }
 
   // Stop on ESC
   if (event->keyboardEvent.type == LDK_KEYBOARD_EVENT_KEY_DOWN
       && event->keyboardEvent.keyCode == LDK_KEYCODE_ESCAPE)
   {
-       ldkEngineStop(0);
+    ldkEngineStop(0);
   }
 
   return true;
@@ -73,24 +73,83 @@ bool onKeyboardEvent(const LDKEvent* event, void* data)
 bool onMouseEvent(const LDKEvent* event, void* data)
 {
   GameState* state = (GameState*) data;
+  const float speed = 0.001f * state->deltaTime;
+  const float lookSpeed = 0.0001f * state->deltaTime;
 
-  if (event->type == LDK_EVENT_TYPE_MOUSE_BUTTON && event->mouseEvent.mouseButton == LDK_MOUSE_BUTTON_LEFT)
+  if (event->type == LDK_EVENT_TYPE_MOUSE_BUTTON)
   {
-    if (event->mouseEvent.type == LDK_MOUSE_EVENT_BUTTON_UP)
-      state->looking = false;
-    else
-      state->looking = true;
+    if (event->mouseEvent.mouseButton == LDK_MOUSE_BUTTON_RIGHT)
+    {
+      if (event->mouseEvent.type == LDK_MOUSE_EVENT_BUTTON_UP)
+        state->looking = false;
+      else
+        state->looking = true;
+    }
+
+    if (event->mouseEvent.mouseButton == LDK_MOUSE_BUTTON_MIDDLE)
+    {
+      if (event->mouseEvent.type == LDK_MOUSE_EVENT_BUTTON_UP)
+        state->strafing = false;
+      else
+        state->strafing = true;
+    }
   }
+
+  Vec3 cam_dir = ldkCameraDirectionNormalized(state->camera);
+  Vec3 side_dir = vec3Normalize(vec3Cross(cam_dir, vec3Up()));
+  Vec3 cam_up = vec3Normalize(vec3Cross(cam_dir, side_dir));
 
   if (state->looking)
   {
-    const float speed = 0.0001f * state->deltaTime;
-    Vec3 cam_dir = ldkCameraDirectionNormalized(state->camera);
     cam_dir.y += -(event->mouseEvent.yRel * speed);
 
-    Vec3 side_dir = vec3Cross(cam_dir, vec3Up());
-    cam_dir = vec3Add(cam_dir, vec3Mul(side_dir, (event->mouseEvent.xRel * speed)));
+    cam_dir = vec3Add(cam_dir, vec3Mul(side_dir, (event->mouseEvent.xRel * lookSpeed)));
     state->camera->target = vec3Add(state->camera->position, vec3Normalize(cam_dir));
+  }
+
+
+  if (state->strafing)
+  {
+    if (abs(event->mouseEvent.yRel) > abs(event->mouseEvent.xRel))
+    {
+      if (event->mouseEvent.yRel < 0)
+      {
+        state->camera->position = vec3Add(state->camera->position, vec3Mul(cam_up, speed));
+        state->camera->target = vec3Add(state->camera->target, vec3Mul(cam_up, speed));
+      }
+      else if (event->mouseEvent.yRel > 0)
+      {
+        state->camera->position = vec3Sub(state->camera->position, vec3Mul(cam_up, speed));
+        state->camera->target = vec3Sub(state->camera->target, vec3Mul(cam_up, speed));
+      }
+    }
+    else
+    {
+      if (event->mouseEvent.xRel < 0)
+      {
+        state->camera->position = vec3Add(state->camera->position, vec3Mul(side_dir, speed));
+        state->camera->target = vec3Add(state->camera->target, vec3Mul(side_dir, speed));
+      }
+      else if (event->mouseEvent.xRel > 0)
+      {
+        state->camera->position = vec3Sub(state->camera->position, vec3Mul(side_dir, speed));
+        state->camera->target = vec3Sub(state->camera->target, vec3Mul(side_dir, speed));
+      }
+    }
+  }
+
+  if (event->type == LDK_EVENT_TYPE_MOUSE_WHEEL)
+  {
+    if (event->mouseEvent.wheelDelta > 0)
+    {
+      state->camera->position = vec3Add(state->camera->position, vec3Mul(cam_dir, 3 * speed));
+      state->camera->target = vec3Add(state->camera->target, vec3Mul(cam_dir, 3 * speed));
+    }
+    else
+    {
+      state->camera->position = vec3Sub(state->camera->position, vec3Mul(cam_dir, 3 * speed));
+      state->camera->target = vec3Sub(state->camera->target, vec3Mul(cam_dir, 3 * speed));
+    }
   }
 
   return true;
@@ -103,7 +162,7 @@ bool onWindowEvent(const LDKEvent* event, void* state)
     ldkEngineStop(0);
   }
   else if (event->windowEvent.type == LDK_WINDOW_EVENT_RESIZED
-  || event->windowEvent.type == LDK_WINDOW_EVENT_MAXIMIZED)
+      || event->windowEvent.type == LDK_WINDOW_EVENT_MAXIMIZED)
   {
     uint32 width = event->windowEvent.width;
     uint32 height = event->windowEvent.height;
@@ -133,8 +192,7 @@ bool onFrameEvent(const LDKEvent* event, void* data)
   return true;
 }
 
-// Engine driven approach
-int pureEngineApplication()
+int main()
 {
   GameState state = {0};
   ldkEngineInitialize();
@@ -153,124 +211,5 @@ int pureEngineApplication()
   state.camera->position = vec3(0.0f, 1.0f, 2.0f);
 
   return ldkEngineRun();
-}
-
-// Pure OS approach
-bool pureOsApplication()
-{
-  ldkOsInitialize();
-  ldkLogInitialize(0, "-- Start --");
-  ldkOsCwdSetFromExecutablePath();
-
-  LDKGCtx context = ldkOsGraphicsContextOpenglCreate(3, 3, 24, 8);
-
-  LDKWindow window = ldkOsWindowCreate("Window 1", 800, 600);
-  ldkOsWindowTitleSet(window, "Hello, Sailor!");
-  ldkOsWindowIconSet(window, "../ldk.ico");
-
-  ldkGraphicsVsyncSet(true);
-
-  LDKWindow window2 = ldkOsWindowCreate("Window 2", 800, 600);
-  ldkOsWindowTitleSet(window2, "Hello, Friend!");
-
-  bool running = true;
-
-  while (running)
-  {
-    LDKEvent e;
-    while(ldkOsEventsPoll(&e))
-    {
-      if (e.type == LDK_EVENT_TYPE_KEYBOARD)
-      {
-        if (e.keyboardEvent.type == LDK_KEYBOARD_EVENT_KEY_DOWN
-            && e.keyboardEvent.keyCode == LDK_KEYCODE_ESCAPE)
-        {
-          ldkLogDebug("ESC Pressed!");
-          running = false;
-          break;
-        }
-      }
-    }
-
-    ldkOsGraphicsContextCurrent(window, context);
-    glClearColor(0, 0, 255, 0);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    ldkOsWindowBuffersSwap(window);
-
-    ldkOsGraphicsContextCurrent(window2, context);
-    glClearColor(0, 127, 255, 0);
-    glClear(GL_COLOR_BUFFER_BIT);
-    ldkOsWindowBuffersSwap(window2);
-  }
-
-  ldkOsWindowDestroy(window);
-  ldkLogTerminate();
-  ldkOsTerminate();
-  return 0;
-}
-
-// Mixed approach
-int mixedModeApplication()
-{
-  ldkEngineInitialize();
-  ldkGraphicsViewportTitleSet("LDK");
-  ldkGraphicsViewportIconSet("ldk.ico");
-  glClearColor(0, 0, 255, 0);
-
-  bool running = true;
-  while (running)
-  {
-    LDKEvent e;
-    while(ldkOsEventsPoll(&e))
-    {
-      if (e.type == LDK_EVENT_TYPE_KEYBOARD)
-      {
-        if (e.keyboardEvent.type == LDK_KEYBOARD_EVENT_KEY_DOWN
-            && e.keyboardEvent.keyCode == LDK_KEYCODE_ESCAPE)
-        {
-          ldkLogDebug("ESC Pressed!");
-          running = false;
-          break;
-        }
-
-      }
-    }
-    // Some manual opengl stuff...
-    glClear(GL_COLOR_BUFFER_BIT);
-    ldkGraphicsSwapBuffers();
-  }
-  ldkEngineTerminate();
-  return 0;
-}
-
-int testPathFunctions()
-{
-  LDKPath path;
-  ldkOsExecutablePathFileNameGet(&path);
-  ldkLogInfo("Exectuable name: %s", path.path);
-  ldkOsExecutablePathGet(&path);
-  ldkLogInfo("Exectuable path : %s", path.path);
-
-  ldkOsExecutablePathFileNameGet(&path);
-  LDKSubStr fileNameSubstr = ldkPathFileNameGetSubstring(path.path);
-  ldkLogInfo("Exectuable path (substring) : %.*s", fileNameSubstr.length, fileNameSubstr.ptr);
-  LDKSubStr fileExtentionSubstr = ldkPathFileExtentionGetSubstring(path.path);
-  ldkLogInfo("Exectuable extention (substring) : %.*s", fileExtentionSubstr.length, fileExtentionSubstr.ptr);
-
-  LDKSmallStr smallStr;
-
-  ldkPathFileNameGet(path.path, smallStr.str, LDK_SMALL_STRING_MAX_LENGTH);
-  ldkLogInfo("Exectuable path (external buffer) : %s", smallStr.str);
-  ldkPathFileExtentionGet(path.path, smallStr.str, LDK_SMALL_STRING_MAX_LENGTH);
-  ldkLogInfo("Exectuable extention (external buffer) : %s", smallStr.str);
-  return 0;
-}
-
-int main()
-{
-  //return testPathFunctions();
-  //return pureOsApplication();
-  //return mixedModeApplication();
-  return pureEngineApplication();
 }
 
