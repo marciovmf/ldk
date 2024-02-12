ad9c9e9ddf321516083bfdc7b564fea1a96a9852
marciovmf <marciovmf@gmail.com>
2024-02-12 18:58:11 +0100

    Separate demos on individual targets

---
+++ demo/demo_mixed_mode.c
@@ -0,0 +1,44 @@
+#include "ldk/ldk.h"
+#include "ldk/module/asset.h"
+#include "ldk/asset/mesh.h"
+#include "ldk/maths.h"
+#include "ldk/common.h"
+
+// Mixed approach
+int mixedModeApplication()
+{
+  ldkEngineInitialize();
+  ldkGraphicsViewportTitleSet("LDK");
+  ldkGraphicsViewportIconSet("ldk.ico");
+  glClearColor(0, 0, 255, 0);
+
+  bool running = true;
+  while (running)
+  {
+    LDKEvent e;
+    while(ldkOsEventsPoll(&e))
+    {
+      if (e.type == LDK_EVENT_TYPE_KEYBOARD)
+      {
+        if (e.keyboardEvent.type == LDK_KEYBOARD_EVENT_KEY_DOWN
+            && e.keyboardEvent.keyCode == LDK_KEYCODE_ESCAPE)
+        {
+          ldkLogDebug("ESC Pressed!");
+          running = false;
+          break;
+        }
+      }
+    }
+    // Some manual opengl stuff...
+    glClear(GL_COLOR_BUFFER_BIT);
+    ldkGraphicsSwapBuffers();
+  }
+  ldkEngineTerminate();
+  return 0;
+}
+
+int main()
+{
+  return mixedModeApplication();
+}
+
---
+++ demo/demo_pure_os_mode.c
@@ -0,0 +1,64 @@
+#include "ldk/ldk.h"
+#include "ldk/module/asset.h"
+#include "ldk/asset/mesh.h"
+#include "ldk/maths.h"
+#include "ldk/common.h"
+
+bool pureOsApplication()
+{
+  ldkOsInitialize();
+  ldkLogInitialize(0, "-- Start --");
+  ldkOsCwdSetFromExecutablePath();
+
+  LDKGCtx context = ldkOsGraphicsContextOpenglCreate(3, 3, 24, 8);
+
+  LDKWindow window = ldkOsWindowCreate("Window 1", 800, 600);
+  ldkOsWindowTitleSet(window, "Hello, Sailor!");
+  ldkOsWindowIconSet(window, "../ldk.ico");
+
+  ldkGraphicsVsyncSet(true);
+
+  LDKWindow window2 = ldkOsWindowCreate("Window 2", 800, 600);
+  ldkOsWindowTitleSet(window2, "Hello, Friend!");
+
+  bool running = true;
+
+  while (running)
+  {
+    LDKEvent e;
+    while(ldkOsEventsPoll(&e))
+    {
+      if (e.type == LDK_EVENT_TYPE_KEYBOARD)
+      {
+        if (e.keyboardEvent.type == LDK_KEYBOARD_EVENT_KEY_DOWN
+            && e.keyboardEvent.keyCode == LDK_KEYCODE_ESCAPE)
+        {
+          ldkLogDebug("ESC Pressed!");
+          running = false;
+          break;
+        }
+      }
+    }
+
+    ldkOsGraphicsContextCurrent(window, context);
+    glClearColor(0, 0, 255, 0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    ldkOsWindowBuffersSwap(window);
+
+    ldkOsGraphicsContextCurrent(window2, context);
+    glClearColor(0, 127, 255, 0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ldkOsWindowBuffersSwap(window2);
+  }
+
+  ldkOsWindowDestroy(window);
+  ldkLogTerminate();
+  ldkOsTerminate();
+  return 0;
+}
+
+int main()
+{
+  return pureOsApplication();
+}
+
