2e76e9f86b1495b906a20f1cc98c436bb8571ec4
marciovmf <marciovmf@gmail.com>
2026-04-13 13:51:05 +0200

    Remove X_IMPL_xxx  macros from ldk_game.h

--- include/ldk_game.h
+++ include/ldk_game.h
@@ -11,28 +11,15 @@
 #define LDK_GAME_H
 
 #include <ldk_common.h>
-
-#if defined(LDK_GAME)
-#if defined(LDK_SHAREDLIB)
-#define X_IMPL_ARRAY
-#define X_IMPL_STRING
-#define X_IMPL_FILESYSTEM
-#define X_IMPL_LOG
-#define X_IMPL_HASHTABLE
-#define X_IMPL_HPOOL
-#endif // LDK_SHAREDLIB
-     
-      
 #include <ldk_argparse.h>
 #include <ldk_os.h>
 #include <ldk_gl.h>
 #include <ldk_entity.h>
 #include <ldk_component.h>
+#include <ldk_system.h>
 #include <ldk_eventqueue.h>
 #include <stdx/stdx_log.h>
-
-#endif // LDK_GAME
-
+#include <stdx/stdx_filesystem.h>
 #include <ldk.h>
 
 #endif //LDK_GAME_H
